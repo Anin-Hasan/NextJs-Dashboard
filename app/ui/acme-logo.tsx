896eb539ff9d536d02646f6773adc53617c1561e
Anin-Hasan <200921@ku.ac.bd>
2024-09-08 02:48:15 +0600

    LittleTuned

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,14 +1,13 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
-import { IdentificationIcon } from "@heroicons/react/20/solid";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <IdentificationIcon className="h-12 w-12 mr-4" />
-      <p className="text-[44px]">200921</p>
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
     </div>
   );
 }
