139a770730a9013de8070afab0f2bfcf93ddfe4c
Anin-Hasan <200921@ku.ac.bd>
2024-09-08 00:29:19 +0600

    ThirdCommit

--- auth.ts
+++ auth.ts
@@ -30,7 +30,10 @@ export const { auth, signIn, signOut } = NextAuth({
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
-          if (passwordsMatch) return user;
+          if (passwordsMatch) {
+            console.log("Successfully authenticated user:", user);
+            return user;
+          }
         }
         console.log("Invalid credentials");
         return null;
